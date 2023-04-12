7e4f66a67e617262922a7d5c66af2079dc72e619
Abdullah Sevindi <abdullahsevindi55@gmail.com>
2023-04-12 22:49:19 +0300

    feature file is added2

---
+++ src/test/java/com/ctis/step_definitions/interaction_stepdef.java
@@ -0,0 +1,89 @@
+package com.ctis.step_definitions;
+
+import io.cucumber.java.en.*;
+import com.ctis.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import org.junit.Assert;
+
+public class interaction_stepdef {
+
+    @When("user make a {string} on others post")
+    public void user_make_a_on_others_post(String string) {
+
+    }
+    @When("user likes on other employees' post")
+    public void user_likes_on_other_employees_post() {
+
+    }
+    @When("user unfollows other employees post")
+    public void user_unfollows_other_employees_post() {
+
+    }
+    @Then("user verifies that the comment is posted")
+    public void user_verifies_that_the_comment_is_posted() {
+
+    }
+    @Then("user verifies that the post is liked")
+    public void user_verifies_that_the_post_is_liked() {
+
+    }
+    @Then("user verifies that the post is unfollowed")
+    public void user_verifies_that_the_post_is_unfollowed() {
+
+    }
+
+
+    @When("user likes his-her own comment")
+    public void user_likes_his_her_own_comment() {
+
+    }
+    @When("user replies his-her own comment")
+    public void user_replies_his_her_own_comment() {
+
+    }
+    @Then("user verifies the comment is liked")
+    public void user_verifies_the_comment_is_liked() {
+
+    }
+    @Then("user verifies the comment is replied")
+    public void user_verifies_the_comment_is_replied() {
+
+    }
+
+
+    @When("user edits his-her comment")
+    public void user_edits_his_her_comment() {
+
+    }
+    @Then("user verifies that his-her comment edited")
+    public void user_verifies_that_his_her_comment_edited() {
+
+    }
+    @Then("user clicks to delete button")
+    public void user_clicks_to_delete_button() {
+
+    }
+    @Then("user sees the comment deleted message")
+    public void user_sees_the_comment_deleted_message() {
+
+    }
+
+
+    @When("user clicks to star icon")
+    public void user_clicks_to_star_icon() {
+
+    }
+    @When("user clicks to filter and search input")
+    public void user_clicks_to_filter_and_search_input() {
+
+    }
+    @When("user chooses Favorites")
+    public void user_chooses_favorites() {
+
+    }
+    @Then("user verifies that the favorite post is seen under the activity stream")
+    public void user_verifies_that_the_favorite_post_is_seen_under_the_activity_stream() {
+
+    }
+}
