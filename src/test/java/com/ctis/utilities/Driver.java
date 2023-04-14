2757471004de398b3b0756210aed76cf016ff4a1
Derya <pinkflower2394@gmail.com>
2023-04-15 00:04:38 +0300

    jenkins

--- src/test/java/com/ctis/utilities/Driver.java
+++ src/test/java/com/ctis/utilities/Driver.java
@@ -7,8 +7,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import java.net.URL;
 
+import java.net.MalformedURLException;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
@@ -52,7 +56,24 @@ public class Driver {
             ChromeOptions options = new ChromeOptions();
 
             switch (browserType){
+
+                case "remote-chrome":
+                    // assign your grid server address
+                    String jenkinsAdress = "34.201.66.167"; // put your own Linux grid IP here
+                    try {
+                        URL url = new URL("http://"+jenkinsAdress+":8081");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("chrome");
+                        driverPool.set(new RemoteWebDriver(url,desiredCapabilities));
+                        driverPool.get().manage().window().maximize();
+                        driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
                 case "chrome":
+
+
 
                     options.addArguments("--remote-allow-origins=*");
                     WebDriverManager.chromedriver().setup();
